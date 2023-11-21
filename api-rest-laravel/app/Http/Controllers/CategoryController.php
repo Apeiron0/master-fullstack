51bd0450e63eec91fd83bdb9fbf1bdf513c8f412
Victor Martinez <studioquetzal@gmail.com>
2023-11-20 23:19:53 -0600

    Added functions to retrive categories, details and store

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -3,11 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('apiauth',['except'=>['index','show']]);
+    }
     //
-    public function pruebas(Request $request){
-        return "Accion de pruebas de category controlller";
+    public function index(){
+        $categories=Category::all();
+        return response()->json([
+            'code'=>200,
+            'status'=>'success',
+            'categories'=>$categories
+        ]);
+    }
+
+    public function show($id){
+        $category=Category::find($id);
+        if(is_object($category)){
+            return response()->json([
+                'code'=>200,
+              'status'=>'success',
+                'category'=>$category
+            ]);
+        }else{
+            return response()->json([
+                'code'=>404,
+              'status'=>'error',
+              'message'=>'Category not found'
+            ]);
+        }
+        
+    
+    }
+
+    public function store(Request $request){
+        //Recoger datos por post
+        $json=$request->input('json',null);
+        $params_array=json_decode($json,true);
+
+        if(!empty($params_array)){
+
+        //Validar datos
+        $validate=\Validator::make($params_array,[
+            'name'=>'required'
+        ]);
+
+        //Guardar la categoria
+        if($validate->fails()){
+            return response()->json([
+                'code'=>400,
+              'status'=>'error',
+              'message'=>$validate->errors()
+            ]);
+        }else{
+            $category=new Category();
+            $category->name=$params_array['name'];
+            $category->save();
+            return response()->json([
+                'code'=>200,
+              'status'=>'success',
+                'category'=>$category
+            ]);
+        }
+        }else{
+            return response()->json([
+                'code'=>400,
+             'status'=>'error',
+             'message'=>'No se envian datos'
+            ]);
+        }
+
+        
+
     }
 }
