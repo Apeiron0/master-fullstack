e07684c083c62ccf7de3f0ba118ad78f3a6be9ee
Victor Martinez <studioquetzal@gmail.com>
2023-11-24 16:40:27 -0600

    Added  post store function

--- api-rest-laravel/app/Http/Controllers/PostController.php
+++ api-rest-laravel/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Helpers\JwtAuth;
 class PostController extends Controller
 {
     //
@@ -34,6 +35,47 @@ class PostController extends Controller
             'code'=>404,
             'status'=>'error',
             'message'=>'Post not found'
+        ]);
+    }
+   }
+
+   public function store(Request $request){
+    //Recoger datos por post
+    $json=$request->input('json',null);
+    $params_array=json_decode($json,true);
+    
+    //conseguir datos usuario
+    $jwtAuth=new JWTAuth();
+    $token=$request->header('Authorization',null);
+    $user=$jwtAuth->checkToken($token,true);
+
+    //validar datos
+    $validate=\Validator::make($params_array,[
+        'title'=>'required',
+        'content'=>'required',
+        'category_id'=>'required',
+        'image'=>'required',
+    ]);
+
+    //guardar datos
+    if($validate->fails()){
+        return response()->json([
+            'code'=>400,
+            'status'=>'error',
+            'message'=>$validate->errors()
+        ]);
+    }else{
+        $post=new Post();
+        $post->user_id=$user->sub;
+        $post->category_id=$params_array['category_id'];
+        $post->title=$params_array['title'];
+        $post->content=$params_array['content'];
+        $post->image=$params_array['image'];
+        $post->save();
+        return response()->json([
+            'code'=>200,
+            'status'=>'success',
+            'post'=>$post
         ]);
     }
    }
