d2b360252842358d1c3d857002fbede8d4ef8107
Victor Martinez <studioquetzal@gmail.com>
2024-02-27 21:50:28 -0600

    Created models for categories, posts and users

---
+++ blog-angular/src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User{
+    constructor(
+        public id:number,
+        public name:string,
+        public surname:string,
+        public role:string,
+        public email:string,
+        public password:string,
+        public descritpion:string,
+        public image:string
+    ){}
+}
